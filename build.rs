b075b3a9f26a7296099a65ba4f556e12fea4b2b6
r8d8 <ckryvomaz@gmail.com>
2017-06-21 08:49:05 +0400

    Invalid gc crate configs Added `std=c99`

--- build.rs
+++ build.rs
@@ -5,16 +5,17 @@ use std::path::PathBuf;
 fn main() {
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
 
-    gcc::compile_library(
-        "libscrypt.a",
-        &[
-            "ext/scrypt/crypto_scrypt.c",
-            "ext/scrypt/crypto_scrypt_smix.c",
-            "ext/scrypt/sha256.c",
-            "ext/scrypt/insecure_memzero.c",
-            "ext/scrypt/warnp.c",
-        ],
-    );
+
+    gcc::Config::new()
+        .flag("-std=c99")
+        .include("ext/scrypt")
+        .file("ext/scrypt/crypto_scrypt.c")
+        .file("ext/scrypt/crypto_scrypt_smix.c")
+        .file("ext/scrypt/sha256.c")
+        .file("ext/scrypt/insecure_memzero.c")
+        .file("ext/scrypt/warnp.c")
+        .compile("libscrypt.a");
+
     println!(
         "cargo:rustc-link-search=native={}",
         out_path.to_str().unwrap()
